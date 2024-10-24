3fd2d705d0f4e3f3d855b684583ef48fcf184419
lautaromdelgado <lautaromdelgado@gmail.com>
2024-10-24 16:20:38 -0300

    Finalización Capitulo 4

--- Capitulo-4/main.go
+++ Capitulo-4/main.go
@@ -48,31 +48,91 @@ import "fmt"
 
 // INTERFACES
 
-type Shape interface {
-	Area() float64
+// type Shape interface {
+// 	Area() float64
+// }
+
+// type Rectangle struct {
+// 	Width  float64
+// 	Height float64
+// }
+
+// func (r Rectangle) Area() float64 {
+// 	return r.Width * r.Height
+// }
+
+// type Circle struct {
+// 	Radius float64
+// }
+
+// func (c Circle) Area() float64 {
+// 	return 3.14 * c.Radius * c.Radius
+// }
+
+// func printArea(s Shape) {
+// 	fmt.Println("Area:", s.Area())
+// }
+
+// func main() {
+// 	r := Rectangle{Width: 10, Height: 5}
+// 	c := Circle{Radius: 7}
+// 	printArea(r)
+// 	printArea(c)
+// }
+
+// Interfaz Vacía
+
+// func printAnything(v interface{}) {
+// 	fmt.Println(v)
+// }
+
+// func main() {
+// 	printAnything(1234)
+// 	printAnything("Hola mundo")
+// 	printAnything(false)
+// }
+
+// TYPES de Interfaces
+
+// func doSomething(i interface{}) {
+// 	switch v := i.(type) {
+// 	case int:
+// 		fmt.Println("Entero:", v)
+// 	case string:
+// 		fmt.Println("Cadena de texto:", v)
+// 	default:
+// 		fmt.Println("No se conoce el tipo de dato..")
+// 	}
+// }
+
+// func main() {
+// 	doSomething(123)
+// 	doSomething("Hola Lautaro")
+// 	doSomething(false)
+// }
+
+type Engine struct {
+	Horsepower int
 }
 
-type Rectangle struct {
-	Width  float64
-	Height float64
+func (e Engine) Start() {
+	fmt.Println("El motor llega a", e.Horsepower, "caballos de potencia.")
 }
 
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
+type Car struct {
+	Make string
+	Model string
+	Engine
 }
 
-type Circle struct {
-	Radius float64
-}
+func main(){
+	car := Car{
+		Make: "Toyota",
+		Model: "Corolla",
+		Engine: Engine{
+			Horsepower: 13500,
+		},
+	}
 
-func (c Circle) Area() float64 {
-	return 3.14 * c.Radius * c.Radius
-}
-
-func printArea(s Shape) {
-	fmt.Println("Area:", s.Area())
-}
-
-func main() {
-	printArea(5)
+	car.Start()
 }
