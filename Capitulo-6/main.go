346a028e8194a27604088df871bfa41180c527a3
Lautaro Delgado <lautaromdelgado@gmail.com>
2024-10-28 10:23:26 -0300

    Quedé minuto 4:00

--- Capitulo-6/main.go
+++ Capitulo-6/main.go
@@ -1,6 +1,6 @@
 /*
 	VIDEO: https://www.youtube.com/watch?v=umGvHL6VssY&list=PLTcOzxm2NcYBBAZC-Ya_xqZ_eZ8i0o9NC&index=6&ab_channel=RobertoMorais
-	MINUTO: 1:41
+	MINUTO: 4:00
 */
 
 // TESTING
@@ -34,31 +34,69 @@ func main() {
 
 // ERRORES PERSONALIZADOS
 
-package main
+// package main
 
-import(
-	"fmt"
-	"errors"
-)
+// import (
+// 	// "errors"
+// 	"fmt"
+// )
 
-var ErrNumberCero = errors.New("No se puede dividir por cero.")
+// var ErrNumberCero = errors.New("No se puede dividir por cero.")
 
-func divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, ErrNumberCero
+// func divide(a, b float64) (float64, error) {
+// 	if b == 0 {
+// 		return 0, ErrNumberCero
+// 	}
+
+// 	return a / b, nil
+// }
+
+// func main() {
+// 	resultado, err := divide(10, 0)
+
+// 	if err != nil {
+// 		if errors.Is(err, ErrNumberCero) {
+// 			fmt.Println("Error personalizado:", err)
+// 			return
+// 		}
+// 	}
+// 	fmt.Println("Resultado de la división es:", resultado)
+// }
+
+// DEFINIR UN ERROR MÁS DESCRIPTIVO
+/*
+type ErrorDetails struct {
+	Arg  float64
+	Prob string
+}
+
+func (e *ErrorDetails) Error() string {
+	return fmt.Sprintf("%f - %s", e.Arg, e.Prob)
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, &ErrorDetails{x, "No puede ser un número negativo."}
 	}
 
-	return	a/b, nil
+	return x * x, nil
 }
 
 func main() {
-	resultado, err := divide(10, 0)
+	_, err := sqrt(-4)
 
 	if err != nil {
-		if errors.Is(err, ErrNumberCero) {
-			fmt.Println("Error personalizado:", err)
-			return
-		}
+		fmt.Println("Error:", err)
 	}
-	fmt.Println("Resultado de la división es:", resultado)
+}
+*/
+
+package main
+
+import (
+	// "fmt"
+)
+
+func main() {
+
 }
